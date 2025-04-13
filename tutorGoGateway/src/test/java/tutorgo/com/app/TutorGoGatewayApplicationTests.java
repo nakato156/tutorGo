19126dde0e8c3a99e1d0f5dd0f4721dbe1aeee09
nakato156 <christianvelasces@gmail.com>
2025-04-13 18:19:06 -0500

    first version api gateway

---
+++ tutorGoGateway/src/test/java/tutorgo/com/app/TutorGoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tutorgo.com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorGoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
